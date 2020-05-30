538c814accf90d3df3c51efeb9af38b8426cbe2b
PaulinaLL <paulinalll90@wp.pl>
2020-05-30 12:14:46 +0200

    Remove CRA defaults & SearchBar and Contact List component + filter

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import ContactList from "./components/Contact-List";
+import SearchBar from "./components/Search-bar";
+import data from "./data/data.json";
 
 function App() {
+  const [inputValue, setInputValue] = useState("");
+
+  const changeHandler = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  const filteredContacts = data.filter((contact) => {
+    return contact.first.toLowerCase().includes(inputValue.toLowerCase());
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <SearchBar inputValue={inputValue} setInputValue={changeHandler} />
+      <ContactList data={filteredContacts} />
     </div>
   );
 }
